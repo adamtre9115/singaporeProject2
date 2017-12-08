c018b4fc6cb8ec54c4605e888c757bb8905fb9e4
Colin Shifley <colin@wifi_stu-10-38-89-36.uncc.edu>
2017-12-07 21:14:03 -0500

    Folder Routes

---
+++ models/index.js
@@ -0,0 +1,22 @@
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/connection.json')[env];
+var db        = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = db;
