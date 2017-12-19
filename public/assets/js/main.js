5d35e152afcc4ba7a6c7c119e0fd52e69b00acd6
Caroline Forsyth <uncforsyth@gmail.com>
2017-12-18 20:05:40 -0500

    monday update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,19 +1,59 @@
 //Character countdown for textarea of Quick Quilly form
-var maxLength = 180;
-$("#quillyMsg").keyup(function () {
-    var currCharCount = $(this).val().length;
-    var charLeft = maxLength - currCharCount;
-    $("#charCount").text(charLeft);
-    if (charLeft <= 20) {
-        $("#charCount").css("color", "red");
-    }
-});
+// var maxLength = 180;
+// $("#quillyMsg").keyup(function () {
+//     var currCharCount = $(this).val().length;
+//     var charLeft = maxLength - currCharCount;
+//     $("#charCount").text(charLeft);
+//     if (charLeft <= 20) {
+//         $("#charCount").css("color", "red");
+//     }
+// });
+
+
+
+var quillyOverlay = $("#popupOverlay");
+var quickQuillyPopup = $("#quickQuillyContainer");
+var randomQuillyPopup = $("#randomQuillyContainer");
 
 
 function loadQuickForm() {
-    var quickBtn = $("#quickQuilly");
-    var quickFormCon = $("#quickContainer");
     $("#quickQuilly").click(function (event) {
-        quickFormCon.style.zIndex = 100;
+        quillyOverlay.style.display = block;
+        quickQuillyPopup.style.display = block;
+    });
+}
+
+function submitQuickForm() {
+    $("#submitQuickQuilly").click(function (event) {
+        quillyOverlay.style.display = none;
+        quickQuillyPopup.style.display = none;
+    });
+}
+
+function cancelQuickForm() {
+    $("#cancelQuickQuilly").click(function (event) {
+        quillyOverlay.style.display = none;
+        quickQuillyPopup.style.display = none;
+    });
+}
+
+function loadRandomForm() {
+    $("#randomQuilly").click(function (event) {
+        quillyOverlay.style.display = block;
+        randomQuillyPopup.style.display = block;
+    });
+}
+
+function submitRandomForm() {
+    $("#submitRandomQuilly").click(function (event) {
+        quillyOverlay.style.display = none;
+        randomQuillyPopup.style.display = none;
+    });
+}
+
+function cancelRandomForm() {
+    $("#cancelRandomQuilly").click(function (event) {
+        quillyOverlay.style.display = none;
+        randomQuillyPopup.style.display = none;
     });
 }
